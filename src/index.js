e5e2541e187d9b64f59f5f65e058b73bc345819c
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-28 00:00:07 +0700

    Make buttons for calling out the contents

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-import "./style.css";
-import data from './data.json';
+import './style.css';
+// import data from './data.json';
 
-const content = document.querySelector("#content");
-console.log(data)
+const content = document.querySelector('#content');
 
+const leftContent = document.createElement('div');
+leftContent.classList.add('left-content');
+content.appendChild(leftContent);
 
+// MID
+const midContent = document.createElement('div');
+midContent.classList.add('mid-content');
+content.appendChild(midContent);
+
+const center = document.createElement('div');
+center.classList.add('center');
+midContent.appendChild(center);
+
+const coverButton = document.createElement('button');
+coverButton.classList.add('cover-button')
+coverButton.innerText = 'NANDEMO\nRESTO';
+center.appendChild(coverButton);
+const menuButton = document.createElement('button');
+menuButton.classList.add('menu-button')
+menuButton.innerText = 'MENU';
+center.appendChild(menuButton);
+
+const rightContent = document.createElement('div');
+rightContent.classList.add('right-content');
+content.appendChild(rightContent);
