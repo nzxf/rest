25a2f9679d2b70a682bcfab09d5b30ebeaefa4b9
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-28 14:59:09 +0700

    Add a signboard and menu buttons

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import './style.css';
 // import data from './data.json';
 
 // Element Maker
-const makeElement = (type, nameClass, parent) => {
+const makeElement = (type, nameClass, parent, text = "") => {
   const element = document.createElement(type);
+  element.innerText = text;
   element.classList.add(nameClass);
   parent.appendChild(element);
   return element;
@@ -11,8 +12,14 @@ const makeElement = (type, nameClass, parent) => {
 
 const content = document.querySelector('#content');
 
-makeElement('div', 'menu-container', content);
+// BOARD
+const boardContainer = makeElement('div', 'board-container', content);
+const board = makeElement('div', 'board', boardContainer, "THIS IS A SIGNBOARD!!!")
+const stickContainer = makeElement('div', 'stick-container', boardContainer)
+const stickBack = makeElement('div', 'stick-back', stickContainer)
+const stickFront = makeElement('div', 'stick-front', stickContainer)
 
+// TRUCK PARTS
 const truckContainer = makeElement('div', 'truck-container', content);
 const truckBody = makeElement('div', 'truck-body', truckContainer);
 const truckFront = makeElement('div', 'truck-front', truckContainer);
@@ -22,7 +29,9 @@ const truckTireBack = makeElement('div', 'truck-tire-back', truckContainer);
 const truckTireFront = makeElement('div', 'truck-tire-front', truckContainer);
 const truckWindows = makeElement('div', 'truck-windows', truckBody);
 const windowLarge = makeElement('div', 'window-large', truckWindows);
-// windowLarge.innerText = "Opening soon"
-const menu = makeElement('div', 'menu', windowLarge);
-menu.inner
 const windowSmall = makeElement('div', 'window-small', truckWindows);
+
+// MENU
+const menu = makeElement('div', 'menu', windowLarge, 'Menu');
+const foodsButton = makeElement('button', 'foods-button', menu, "Foods")
+const drinksButton = makeElement('button', 'drinks-button', menu, "Drinks")
