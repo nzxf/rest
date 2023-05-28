7dc901d78df4a433e1cb8a6de7937810753715c7
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-29 00:10:29 +0700

    Add colorful roof

--- src/index.js
+++ src/index.js
@@ -11,29 +11,36 @@ const makeElement = (type, nameClass, parent, text = '') => {
 };
 
 // Clear Element Children
-const clear = element => {
-  while (element.hasChildNodes()){
-    element.removeChild(element.firstChild)
+const clear = (element) => {
+  while (element.hasChildNodes()) {
+    element.removeChild(element.firstChild);
   }
-}
+};
 
 // Display Menu
 const displayMenu = (title, array, parent) => {
   const menuTitle = makeElement('div', 'menu-title', parent);
-  menuTitle.innerText = title.toUpperCase()
-  const menuContainer = makeElement('div', 'menu-container', parent)
+  menuTitle.innerText = title.toUpperCase();
+  const menuContainer = makeElement('div', 'menu-container', parent);
   for (let i = 0; i < array.length; i++) {
     const menus = makeElement('div', `menus`, menuContainer);
-    makeElement('div', 'menu-name', menus, array[i].name)
-    makeElement('div', 'menu-description', menus, array[i].description)
-    makeElement('div', 'menu-price', menus, `$${array[i].price}`)
+    makeElement('div', 'menu-name', menus, array[i].name);
+    makeElement('div', 'menu-description', menus, array[i].description);
+    makeElement('div', 'menu-price', menus, `$${array[i].price}`);
   }
 };
 
 const content = document.querySelector('#content');
 // BOARD
 const boardContainer = makeElement('div', 'board-container', content);
-const board = makeElement('div','board', boardContainer,`${data.name}\n${data.time}`);
+const board = makeElement('div', 'board', boardContainer);
+const displayDefault = (array) => {
+  makeElement('div', 'resto-name', board, array.name);
+  makeElement('div', 'resto-address', board, array.address);
+  makeElement('div', 'resto-phone', board, array.phone);
+  makeElement('div', 'resto-website', board, array.website);
+};
+displayDefault(data)
 const stickContainer = makeElement('div', 'stick-container', boardContainer);
 const stickBack = makeElement('div', 'stick-back', stickContainer);
 const stickFront = makeElement('div', 'stick-front', stickContainer);
@@ -48,20 +55,40 @@ const truckTireBack = makeElement('div', 'truck-tire-back', truckContainer);
 const truckTireFront = makeElement('div', 'truck-tire-front', truckContainer);
 const truckWindows = makeElement('div', 'truck-windows', truckBody);
 const windowLarge = makeElement('div', 'window-large', truckWindows);
+const windowLargeTop = makeElement('div', 'window-large-top', windowLarge);
+const windowLargeBottom = makeElement(
+  'div',
+  'window-large-bottom',
+  windowLarge
+);
 const windowSmall = makeElement('div', 'window-small', truckWindows);
 
-// MENU
-const menu = makeElement('div', 'menu', windowLarge, 'Menu');
-
-const makeMenuButtons = (array, parent) => {
-  let allMenu = ["burgers", "pizzas", "fries", "drinks"]
-  for (let i = 0; i < allMenu.length; i++) {
-    const element = makeElement('button', 'menu-buttons', parent, allMenu[i].toUpperCase())
-    element.addEventListener('click', function() {
-      clear(board)
-      displayMenu(allMenu[i], array[allMenu[i]], board)
-    })
+// ROOF
+const makeRoof = (howmany) => { 
+  for (let i = 0; i < howmany; i++) {
+    makeElement('div', `roof-${i}`, windowLargeTop);
   }
-}
+};
+makeRoof(6);
 
-makeMenuButtons(data, menu)
+// MENU
+const menu = makeElement('div', 'menu', windowLargeBottom, 'MENU');
+const makeMenuButtons = (array, parent) => {
+  let allMenu = ['burgers', 'pizzas', 'fries', 'drinks'];
+  for (let i = 0; i < allMenu.length; i++) {
+    const element = makeElement(
+      'button',
+      'menu-buttons',
+      parent,
+      allMenu[i].toUpperCase()
+    );
+    element.addEventListener('click', function () {
+      clear(board);
+      displayMenu(allMenu[i], array[allMenu[i]], board);
+    });
+  }
+};
+makeMenuButtons(data, menu);
+
+// CHEF
+const img = makeElement('div', 'img', windowLargeBottom)
