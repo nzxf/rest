37337694a2742f2ba143bd5ba7485d1791308753
nzxf <122195155+nzxf@users.noreply.github.com>
2023-05-29 00:07:44 +0700

    Add font for sign display and menu

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i
+      },
     ],
   },
 };
